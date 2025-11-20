d84b12384d0aa8228696ab0bbdfab17605da88b6
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-20 08:07:57 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,6 +12,7 @@ export ICON=/usr/share/icons/hicolor/scalable/apps/it.mijorus.gearlever.svg
 export DESKTOP=/usr/share/applications/it.mijorus.gearlever
 export DEPLOY_SYS_PYTHON=1
 export DEPLOY_OPENGL=1
+export STARTUPWMCLASS=gnome-calculator # For Wayland, this is 'it.mijorus.gearlever', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Deploy dependencies
 quick-sharun /usr/bin/gearlever \
