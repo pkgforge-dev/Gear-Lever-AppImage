9ab1d617f654b0021a2b32691df00d1707548224
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-22 20:25:34 +0100

    Set `PATH_MAPPING` for lib directory

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,20 +14,21 @@ export DEPLOY_SYS_PYTHON=1
 export DEPLOY_OPENGL=1
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-4.0
-export PATH_MAPPING='/usr/share/gearlever:${SHARUN_DIR}/share/gearlever'
+export PATH_MAPPING='
+       /usr/share/gearlever:${SHARUN_DIR}/share/gearlever
+       /usr/lib/gearlever:${SHARUN_DIR}/lib/gearlever
+'
 export EXEC_WRAPPER=1
 export DEPLOY_LOCALE=1
 export STARTUPWMCLASS=gearlever # For Wayland, this is 'it.mijorus.gearlever', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
 # Deploy dependencies
 quick-sharun /usr/bin/gearlever \
-             /usr/lib/gearlever \
              /usr/lib/libgirepository* \
              /usr/bin/dwarfs \
              /usr/bin/mkdwarfs \
              /usr/bin/dwarfsextract \
              /usr/bin/dwarfsck \
-             /usr/bin/unsquashfs \
              /usr/share/icons/hicolor # Icons are missing on Fedora, so deploying all icons gets rid of this problem
 
 # Turn AppDir into AppImage
