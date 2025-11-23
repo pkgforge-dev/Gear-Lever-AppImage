e7e7cf608d3d4d06018495b0cbf4471cdeaa70a9
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-23 19:51:31 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,9 +9,6 @@ echo "---------------------------------------------------------------"
 make-aur-package dwarfs-bin
 make-aur-package gearlever
 pacman -Syu --noconfirm coreutils binutils gawk 7zip
-# Upstream 'file' is bugged in not respecting the GCONV_PATH and not working
-pacman -Rsndd --noconfirm file
-make-aur-package file-git
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
