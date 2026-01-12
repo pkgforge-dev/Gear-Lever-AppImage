ffdb76580616fe15dd7a54ee21bcbf4f59dbe302
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-12 07:36:15 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-make-aur-package dwarfs-bin
+make-aur-package dwarfs
 make-aur-package python-desktop-entry-lib
 make-aur-package python-ftputil
 make-aur-package gearlever
