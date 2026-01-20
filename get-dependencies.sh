35eae97b9069730f289cd118c1832a16c7df7e0b
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-20 19:38:54 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,14 +4,14 @@ set -eu
 
 ARCH=$(uname -m)
 
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+get-debloated-pkgs --add-common --prefer-nano
+pacman -Syu --noconfirm coreutils binutils gawk
+
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 make-aur-package dwarfs-bin
 make-aur-package python-desktop-entry-lib
 make-aur-package python-ftputil
 make-aur-package gearlever
-pacman -Syu --noconfirm coreutils binutils gawk
-
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
